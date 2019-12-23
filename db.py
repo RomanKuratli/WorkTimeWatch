1946984306ee909f59597be9b1dd097c345c25a2
RomanKuratli <35655993+RomanKuratli@users.noreply.github.com>
2019-12-23 22:16:22 +0100

    Settings page

--- db.py
+++ db.py
@@ -37,5 +37,6 @@ def get_params():
 
     return ret
 
+
 if __name__ == "__main__":
     print(f"get_params(): {get_params()}")
--- static/settings.js
+++ static/settings.js
@@ -1,24 +1,44 @@
 function settingsInit() {
     // initialization code, called from openModule()
     console.log('settingsInit() called!');
+    let paramId = 0;
     let paramsTableBody = document.getElementById('paramsTableBody');
+    let paramsTableSubmit = document.getElementById('paramsTableSubmit');
+    paramsTableSubmit.onclick = onSubmitAppParamChangeForm;
+
+    // fetch application parameters
     fetch(/*'http://localhost:5000*/ '/params', {
         method: 'GET',
         mode: 'no-cors'
     })
-    .then(response => {
-        console.log('response: ', response);
-        return response.json();
-    })
+    .then(response => response.json())
     .then(json => {
+        // Empty the table body first
+        paramsTableBody.innerHTML = '';
+
         for (param of json) {
+            paramId++;
+
             let row = document.createElement('tr');
 
             let keyCell = row.insertCell();
-            keyCell.innerText = param.key;
+            let keyId = paramId + '_key';
+            let keyInput = document.createElement("input");
+            keyInput.setAttribute("id", keyId);
+            keyInput.setAttribute("name", keyId);
+            keyInput.setAttribute("type", "text");
+            keyInput.setAttribute("disabled", "disabled");
+            keyInput.setAttribute("value", param.key);
+            keyCell.appendChild(keyInput);
 
             let valueCell = row.insertCell();
-            valueCell.appendChild(createAppParamChangeForm(param.key, param.value));
+            let valueId = paramId + '_value';
+            let valueInput = document.createElement("input");
+            valueInput.setAttribute("id", valueId);
+            valueInput.setAttribute("name", valueId);
+            valueInput.setAttribute("type", "text");
+            valueInput.setAttribute("value", param.value);
+            valueCell.appendChild(valueInput);
 
             let typeCell = row.insertCell();
             typeCell.innerText = param.type;
@@ -29,30 +49,22 @@ function settingsInit() {
 }
 
 function onSubmitAppParamChangeForm(event) {
-    event.preventDefault();
     console.log('submit clicked, event: ', event);
-}
 
-function createAppParamChangeForm(key, currentValue) {
-    let form = document.createElement('form');
-    form.setAttribute('method', 'POST');
-    form.setAttribute('action', '/params');
-    
-    let keyField = document.createElement('input');
-    keyField.setAttribute('type', 'hidden');
-    keyField.setAttribute('value', key);
-    form.appendChild(keyField);
+    let newValues = {};
+    let i = 1;
+    while (true) {
+        console.log("looking for field with id", i.toString() + "_key");
+        let keyField = document.getElementById(i.toString() + "_key");
+        if (null == keyField) break;
+        let key = keyField.value;
+        let val = document.getElementById(i.toString() + "_value").value;
+        newValues[key] = val;
+        i++;
+    }
 
-    let valueField = document.createElement('input');
-    valueField.setAttribute('type', 'text');
-    valueField.setAttribute('value', currentValue);
-    form.appendChild(valueField);
+    console.log("call parameters POST /config: ", newValues);
 
-    let changeBtn = document.createElement('button');
-    changeBtn.setAttribute('type', 'submit');
-    changeBtn.setAttribute('value', 'Change');
-    changeBtn.onclick = onSubmitAppParamChangeForm;
-    form.appendChild(changeBtn);
-
-    return form;
+    event.preventDefault();
+    return false;
 }
